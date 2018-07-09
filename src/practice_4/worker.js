20a4fea8a29d2ec2c672ae0122812622773caa9b
beeMonkey <675383749@qq.com>
2018-07-09 16:16:15 +0800

    finish the pratice 1 to 6 and build the test for each one

--- src/practice_4/worker.js
+++ src/practice_4/worker.js
@@ -1,1 +1,12 @@
+import Person from "../../src/practice_2/person.js";
+class Worker extends Person{
+    constructor(name,age){
+        super(name,age);
+    }
+    introduce(){
+        return super.introduce()+" I am a Worker. I have a job."
+    }
+}
+export default Worker
+
 
